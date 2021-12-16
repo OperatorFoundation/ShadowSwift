2cfac0760d0ef429ec8d6cf8c4a2ed944d170321
consuelita <adelita@operatorfoundation.org>
2021-12-16 15:04:36 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -64,7 +64,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/NetworkLinux.git", from: "0.4.5"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", from: "1.2.5"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.4"),
+        .package(url: "https://github.com/OperatorFoundation/Transmission.git", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.5"),
         .package(url: "https://github.com/apple/swift-crypto", from: "2.0.0")
     ],
