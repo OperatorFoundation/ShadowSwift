cdf1cfb9195472bc71e0f8440b6cf7afa056f849
consuelita <adelita@operatorfoundation.org>
2022-03-01 13:23:16 -0600

    Update Bloom.swift

--- Sources/ShadowSwift/Bloom.swift
+++ Sources/ShadowSwift/Bloom.swift
@@ -8,41 +8,60 @@
 import Foundation
 import Transport
 
-public struct BloomFilter<Data: Hashable> {
+public struct BloomFilter<Data: Hashable>: Codable
+{
     private var data: [Bool]
     private let seeds: [Int]
     
-    public init(size: Int, hashCount: Int) {
+    public init(size: Int, hashCount: Int)
+    {
         data = Array(repeating: false, count: size)
         seeds = (0..<hashCount).map({ _ in Int.random(in: 0..<Int.max) })
     }
     
-    init() {
+    init()
+    {
         self.init(size: 512, hashCount: 3)
     }
     
-    mutating func insert(_ salt: Data) {
+    mutating func insert(_ salt: Data)
+    {
         hashes(for: salt)
         .forEach({ hash in
             data[hash % data.count] = true
         })
     }
     
-    func contains(_ salt: Data) -> Bool {
+    func contains(_ salt: Data) -> Bool
+    {
         return hashes(for: salt)
         .allSatisfy({ hash in
             data[hash % data.count]
         })
     }
     
-    private func hashes(for salt: Data) -> [Int] {
-        return seeds.map({ seed -> Int in
+    private func hashes(for salt: Data) -> [Int]
+    {
+        return seeds.map(
+            { seed -> Int in
             var hasher = Hasher()
             hasher.combine(salt)
             hasher.combine(seed)
             let hashValue = abs(hasher.finalize())
             return hashValue
         })
+    }
+    
+    // Use Codable to save as JSON
+    func save(filename: String)
+    {
+        
+    }
+    
+    // Use Codable to load from JSON
+    func load(filename: String)
+    {
+        
     }
 }
 
