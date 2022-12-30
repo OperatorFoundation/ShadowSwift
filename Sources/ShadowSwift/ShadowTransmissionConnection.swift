eebd138b8b0c94d0f54a39a4cc94177212cf021a
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-12-29 18:07:39 -0600

    Fixed a bug in readWithPrefixLength() and also refactored it to use TransmissionTypes.readWithPrefixLength() in the implementation

--- Sources/ShadowSwift/ShadowTransmissionConnection.swift
+++ Sources/ShadowSwift/ShadowTransmissionConnection.swift
@@ -164,36 +164,7 @@ public class ShadowTransmissionClientConnection: Transmission.Connection
     
     public func readWithLengthPrefix(prefixSizeInBits: Int) -> Data?
     {
-        let prefixSizeInBytes = prefixSizeInBits * 8
-        guard let lengthData = self.read(size: prefixSizeInBytes) else
-        {
-            return nil
-        }
-        
-        let length: Int
-        switch prefixSizeInBits
-        {
-            case 8:
-                let uint8 = UInt8(data: lengthData)
-                length = Int(uint8)
-            
-            case 16:
-                let uint16 = UInt16(data: lengthData)
-                length = Int(uint16)
-            
-            case 32:
-                let uint32 = UInt32(data: lengthData)
-                length = Int(uint32)
-            
-            case 64:
-                let uint64 = UInt64(data: lengthData)
-                length = Int(uint64)
-            
-            default:
-                return nil
-        }
-        
-        return self.read(size: length)
+        return TransmissionTypes.readWithLengthPrefix(prefixSizeInBits: prefixSizeInBits, connection: self)
     }
     
     public func write(string: String) -> Bool
@@ -230,53 +201,8 @@ public class ShadowTransmissionClientConnection: Transmission.Connection
         if networkClosed {
             return false
         }
-        
-        let dataSize = data.count
-        switch prefixSizeInBits
-        {
-            case 8:
-                guard dataSize < UInt8.max else
-                {
-                    return false
-                }
-                
-                let uint8 = UInt8(dataSize)
-                let newData = uint8.data + data
-                return self.write(data: newData)
-                
-            case 16:
-                guard dataSize < UInt16.max else
-                {
-                    return false
-                }
-                
-                let uint16 = UInt16(dataSize)
-                let newData = uint16.data + data
-                return self.write(data: newData)
-                
-            case 32:
-                guard dataSize < UInt32.max else
-                {
-                    return false
-                }
-                
-                let uint32 = UInt32(dataSize)
-                let newData = uint32.data + data
-                return self.write(data: newData)
-                
-            case 64:
-                guard dataSize < UInt64.max else
-                {
-                    return false
-                }
-                
-                let uint64 = UInt64(dataSize)
-                let newData = uint64.data + data
-                return self.write(data: newData)
-                
-            default:
-                return false
-        }
+
+        return TransmissionTypes.writeWithLengthPrefix(data: data, prefixSizeInBits: prefixSizeInBits, connection: self)
     }
     
     public func close()
