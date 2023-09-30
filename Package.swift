9c20258b1dbfc6f4dd4ba1c3ada0bc7d34e103c3
bragelbytes <joebragel@gmail.com>
2023-09-30 14:15:23 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["ShadowSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.3"),
         .package(url: "https://github.com/apple/swift-crypto", from: "2.5.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
 
