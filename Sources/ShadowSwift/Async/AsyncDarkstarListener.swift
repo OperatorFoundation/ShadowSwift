a61c2d2bca1315b3db800e6d462d82a03a823b8d
bragelbytes <joebragel@gmail.com>
2024-02-01 11:23:34 -0800

    Update AsyncDarkstarListener.swift

--- Sources/ShadowSwift/Async/AsyncDarkstarListener.swift
+++ Sources/ShadowSwift/Async/AsyncDarkstarListener.swift
@@ -18,13 +18,19 @@ public class AsyncDarkstarListener: AsyncListener
     let logger: Logger
     let networkListener: AsyncListener
 
-    public init(config: ShadowConfig.ShadowServerConfig, logger: Logger, verbose: Bool = false) throws
+    public convenience init(config: ShadowConfig.ShadowServerConfig, logger: Logger, verbose: Bool = false) throws
+    {
+        let asyncTcpSocketListener = try AsyncTcpSocketListener(host: config.serverIP, port: Int(config.serverPort), logger, verbose: verbose)
+        self.init(config: config, listener: asyncTcpSocketListener, logger: logger, verbose: verbose)
+    }
+    
+    public init(config: ShadowConfig.ShadowServerConfig, listener: AsyncListener, logger: Logger, verbose: Bool = false)
     {
         self.host = config.serverIP
         self.port = Int(config.serverPort)
         self.config = config
         self.logger = logger
-        self.networkListener = try AsyncTcpSocketListener(host: self.host, port: self.port, self.logger, verbose: verbose)
+        self.networkListener = listener
     }
 
     public func accept() async throws -> AsyncConnection
