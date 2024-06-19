9c4c3dcb6aac5bb7cfb918899ca3f633b16e6846
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-06-19 12:24:26 -0500

    Updated swift-tools and platforms

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ShadowSwift",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v14),
         .iOS(.v16)
     ],
     products: [
